86a217e1e655b2cb5bc74acfdd41e42613cbee30
Huzaifa Zainuddin <huzaifazainuddin@A005753.local>
2017-03-07 22:18:38 -0600

    Added additional fields

--- copytos3.py
+++ copytos3.py
@@ -10,11 +10,14 @@ as3 = boto3.resource('s3')
 class logs(Document):
     fullPath = StringField(required=True, max_length=200)
     fileName = StringField(required=True, max_length=200)
+    objectId = StringField(required=True, max_length=200)
+    taskName = StringField()
     url = StringField()
     dateCreated = DateTimeField()
     errorCount = IntField()
 
 class jobs(Document):
+    objectId = StringField(required=True, max_length=200)
     taskName = StringField(required=True, max_length=200)
 
 class error(Document):
@@ -28,30 +31,36 @@ def lambda_handler(event, context):
     connect('hztest', host='ec2-52-91-110-103.compute-1.amazonaws.com', port=27017)
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+    key = event['Records'][0]['s3']['object']['key']
     try:
         obj = as3.Object(bucket, key)
         fileName=key[24::]
         sitePrefix='https://s3.amazonaws.com/informatica-logs/'
         url=sitePrefix+key
-        dataFile=obj.get()["Body"].read().decode('utf8')
-        if dataFile.count('/n')>0:
+        dataFile=obj.get()['Body'].read().decode('utf8')
+        objectId=key[30:50]
+        if dataFile.count('\n') > 0:
             cnt=(dataFile.count('\n')-1)
         else: 
-            cnt=0
+            cnt=(dataFile.count('\n'))
         lastModified=obj.get()['ResponseMetadata']['HTTPHeaders']['x-amz-meta-lastwritetime']
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
     try:
-         post=logs(fullPath = key,
-                       fileName = fileName,
-                       url = url,
-                       dateCreated = lastModified,
-                       errorCount = cnt)
-         post.save()
-         success= print(post)
-         return success
+        jobsobj=jobs.objects.get(objectId=objectId)
+        post=logs(
+            fullPath = key,
+            fileName = fileName,
+            objectId = objectId,
+            taskName = jobsobj.taskName,
+            url = url,
+            dateCreated = lastModified,
+            errorCount = cnt
+            )
+        post.save()
+        success= print('Metadata for file: '+post.fileName+' loaded to mongodb')
+        return success
     except Exception as z:
         raise z
